423843327a21d24df602d7683da9151d035e69ff
subject026 <subject026@protonmail.com>
2024-05-24 20:49:09 +0200

    initial commit

---
+++ src/app/components/Providers.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: ReactNode }) {
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
