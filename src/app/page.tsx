c9f3e8383dc82cb956bee75f874bb4224e3686f1
subject026 <subject026@protonmail.com>
2024-05-26 13:10:28 +0200

    replace qr code etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,8 +81,8 @@ export default function Home() {
           height="40"
         />
       </header>
-      <main className={clsx(PAGE_WRAP, "grow")}>
-        <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-8">
+      <main className={clsx(PAGE_WRAP, "grow lg:pt-6")}>
+        <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-16">
           <section className="grid gap-4 pb-8 lg:col-span-1 lg:col-start-1 lg:pb-0">
             <h1 className="text-4xl font-bold tracking-[-.02em]">
               Fund evacuations from gaza with crypto
@@ -92,22 +92,29 @@ export default function Home() {
             </h3>
           </section>
 
-          <section className="lg:order-3 lg:col-span-1 lg:col-start-2 lg:bg-white lg:rounded-[2rem] flex items-center justify-center py-16 lg:row-span-2">
-            <div>
-              <div className="grid justify-center">
-                <span className="px-4 py-2 font-medium text-xl rounded-full bg-white text-black">
-                  ethevacuations.eth
-                </span>
-                <CopyAddress />
-              </div>
-              <div className="flex justify-center">
-                <Image src="/qr_code.png" alt="hero" width="190" height="189" />
+          <section className="lg:order-3 lg:col-span-1 lg:col-start-2 lg:row-span-2">
+            <div className="lg:bg-white lg:rounded-[2rem] flex items-center justify-center lg:py-16">
+              <div>
+                <div className="grid justify-center">
+                  <span className="px-4 py-2 font-medium text-xl rounded-full bg-white text-black">
+                    ethevacuations.eth
+                  </span>
+                  <CopyAddress />
+                </div>
+                <div className="flex justify-center pt-4">
+                  <Image
+                    src="/qr_code.jpg"
+                    alt="hero"
+                    width="190"
+                    height="189"
+                  />
+                </div>
               </div>
             </div>
           </section>
 
-          <section className="lg:order-4 lg:">
-            <div className="grid grid-cols-2 pt-6">
+          <section className="lg:order-4 pb-4">
+            <div className="grid grid-cols-2 pt-6 pb-4">
               <div className="grid justify-center text-center">
                 <h3 className="text-neutral-500 font-medium text-xl">
                   Total Raised
